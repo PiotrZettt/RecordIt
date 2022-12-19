029002da4e559b06e72a07c90ca9e17da1e2d760
PiotrZettt <piotrzielinski@o2.pl>
2022-12-19 19:40:55 +0000

    initial commit

--- RecordIt/settings.py
+++ RecordIt/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['recordIt-env.eba-5zus2pwk.us-west-2.elasticbeanstalk.com', '127.0.0.1']
+ALLOWED_HOSTS = ['Recordit-env.eba-4dn23bew.eu-west-2.elasticbeanstalk.com', '127.0.0.1']
 
 
 # Application definition
